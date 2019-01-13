03d132cdaddb73c40d5418b600b7823261c837e7
Shubham Agarwal <Shubhamagarwal1616@gmail.com>
2019-01-13 15:35:59 +0530

    changes for extracting column[zero]

--- lib/Matrix.rb
+++ lib/Matrix.rb
@@ -7,7 +7,6 @@ class Matrix
   end
 
   def extract_row(index)
-
     matrix = @string.split("\n")
     @no_of_rows = matrix.size
     for each in (0..@no_of_rows-1) do
@@ -17,6 +16,18 @@ class Matrix
     row = []
     for each in (0..@no_of_column-1) do
       row << matrix[index][each].to_i
+    end
+    row
+  end
+
+  def extract_column(index)
+    matrix = @string.split("\n")
+    for each in (0..3) do
+      matrix[each] = matrix[each].split(" ")
+    end
+    row = []
+    for each in (0..4-1) do
+      row << matrix[each][0].to_i
     end
     row
   end
--- spec/Matrix_spec.rb
+++ spec/Matrix_spec.rb
@@ -12,7 +12,13 @@ describe Matrix do
       matrix = Matrix.new("9 8 7\n19 18 17")
       expect(matrix.extract_row(1)).to eql([19, 18, 17])
     end
+  end
 
+  context "test for extracting a column from a matrix given its index" do
+    it "column[0] of '1 2 3 ; 4 5 6 ; 7 8 9 ; 8 7 6' is [1, 4, 7, 8]" do
+      matrix = Matrix.new("1 2 3\n4 5 6\n7 8 9\n 8 7 6")
+      expect(matrix.extract_column(0)).to eql([1, 4, 7, 8])
+    end
   end
 
 end
